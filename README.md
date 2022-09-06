2da2e7e9c91e0ef4aac12b40a9c28a2f4d70ca20
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2022-09-06 13:45:39 +0530

    completed logical part

---
+++ README.md
@@ -0,0 +1,2 @@
+This app take user input such as bill amount and cash given and help the user by telling minimum number of notes required for the change.
+This app is built in HTML, CSS and Vanila JS.
--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-msg")
+const message = document.querySelector("#error-msg");
+const totalNotes = document.querySelectorAll(".total-notes");
+const notes = [2000, 500, 100, 50, 20, 10, 5, 1];
+
+
 checkButton.addEventListener("click", function checkBillAndCashAmount(){
     hideMessage();
     
     if(billAmount.value>0){
         if(cashGiven.value>=billAmount.value){
-
+            const returedAmount = cashGiven.value - billAmount.value;
+            calculatechange(returedAmount);
         }
         else{
             showMessage("put valid cash value");
@@ -19,11 +24,22 @@ checkButton.addEventListener("click", function checkBillAndCashAmount(){
 
     }
 });
-function hideMessage(){
+
+ function hideMessage(){
     message.style.display = "none";
 }
+
  function showMessage(msg){
     message.style.display = "block";
      message.innerText = msg;
 
+ }
+
+ function calculatechange(returedAmount){
+    for(let i=0;i<notes.length;i++){
+        const numberOfNotes = Math.trunc(returedAmount/notes[i]);
+        returedAmount %= notes[i];
+        totalNotes[i].innerText = numberOfNotes;
+
+    }
  }
